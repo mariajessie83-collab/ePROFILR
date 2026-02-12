0b0c100746666fd827a0be5ce8379fcd64ae8663
angelocallocallo1111@gmail.com <angelocallocallo1111@gmail.com>
2026-02-12 20:06:28 +0800

    app

--- Gsystem/Components/Appsettings.cs
+++ Gsystem/Components/Appsettings.cs
@@ -2,7 +2,7 @@
 {
     public static class Appsettings
     {
-        public static string BaseUrl = "https://eprofilr-1.onrender.com/";
+        public static string BaseUrl ="https://eprofilr-1.onrender.com/";
         //public static string BaseUrl = "https://localhost:7111/";
         //https://servertest-3-ye5g.onrender.com/
         //https://servertest-t5g2.onrender.com/
